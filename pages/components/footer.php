0578a0195d76495b6fbb46b3145926b398fba5ee
SilvaUnCompte <silvablocks@gmail.com>
2023-03-16 15:24:36 +0100

    archi

---
+++ pages/components/footer.php
@@ -0,0 +1,109 @@
+<!-- Footer -->
+<footer class="text-center text-lg-start bg-dark text-muted">
+        <!-- Section: Social media -->
+        <section id="top_bar_footer" class="d-flex justify-content-center justify-content-lg-between p-4 border-bottom">
+            <!-- Left -->
+            <div class="footer_text me-5 d-none d-lg-block">
+                <span>Get connected with us on social networks:</span>
+            </div>
+            <!-- Left -->
+
+            <!-- Right -->
+            <div id="media_icon_div">
+                <a id="link_media_icon_facebook" href="" class="me-4 text-reset">
+                    <i id="media_icon_facebook" class="fab fa-facebook-f"></i>
+                </a>
+                <a id="link_media_icon_twitter" href="" class="me-4 text-reset">
+                    <i id="media_icon_twitter" class="fab fa-twitter"></i>
+                </a>
+                <a id="link_media_icon_instagram" href="" class="me-4 text-reset">
+                    <i id="media_icon_instagram" class="fab fa-instagram"></i>
+                </a>
+                <a id="link_media_icon_linkedin" href="" class="me-4 text-reset">
+                    <i id="media_icon_linkedin" class="fab fa-linkedin"></i>
+                </a>
+                <a id="link_media_icon_github" href="" class="me-4 text-reset">
+                    <i id="media_icon_github" class="fab fa-github"></i>
+                </a>
+            </div>
+            <!-- Right -->
+        </section>
+        <!-- Section: Social media -->
+
+        <!-- Section: Links  -->
+        <section class="">
+            <div class="container text-center text-md-start mt-5">
+                <!-- Grid row -->
+                <div class="row mt-3">
+                    <!-- Grid column -->
+                    <div class="footer_text col-md-3 col-lg-4 col-xl-3 mx-auto mb-4">
+                        <!-- Content -->
+                        <h6 class="text-uppercase fw-bold mb-4">
+                            <i class="fas fa-gem me-3"></i>Company name
+                        </h6>
+                        <p>
+                            Here you can use rows and columns to organize your footer content. Lorem ipsum
+                            dolor sit amet, consectetur adipisicing elit.
+                        </p>
+                    </div>
+                    <!-- Grid column -->
+
+                    <!-- Grid column -->
+                    <div class="col-md-2 col-lg-2 col-xl-2 mx-auto mb-4 links">
+                        <!-- Links -->
+                        <h6 class="text-uppercase fw-bold mb-4">
+                            Products
+                        </h6>
+                        <p>
+                            <a href="#!" class="text-reset">Angular</a>
+                        </p>
+                        <p>
+                            <a href="#!" class="text-reset">React</a>
+                        </p>
+                        <p>
+                            <a href="#!" class="text-reset">Vue</a>
+                        </p>
+                        <p>
+                            <a href="#!" class="text-reset">Laravel</a>
+                        </p>
+                    </div>
+                    <!-- Grid column -->
+
+                    <!-- Grid column -->
+                    <div class="col-md-3 col-lg-2 col-xl-2 mx-auto mb-4 links">
+                        <!-- Links -->
+                        <h6 class="text-uppercase fw-bold mb-4">
+                            Useful links
+                        </h6>
+                        <p>
+                            <a href="#!" class="text-reset">Pricing</a>
+                        </p>
+                        <p>
+                            <a href="#!" class="text-reset">Settings</a>
+                        </p>
+                        <p>
+                            <a href="#!" class="text-reset">Orders</a>
+                        </p>
+                        <p>
+                            <a href="#!" class="text-reset">Help</a>
+                        </p>
+                    </div>
+                    <!-- Grid column -->
+                </div>
+                <!-- Grid row -->
+            </div>
+        </section>
+        <!-- Section: Links  -->
+
+        <!-- Copyright -->
+        <div class="text-center p-4" style="background-color: rgba(0, 0, 0, 0.05);">
+            © 2021 Copyright:
+            <a class="text-reset fw-bold" href="https://mdbootstrap.com/">MDBootstrap.com</a>
+        </div>
+        <!-- Copyright -->
+    </footer>
+    <!-- Footer -->
+
+    <!------------------------ Bootstrap Bundle with Popper ------------------------>
+    <script src="../assets/vendors/jquery/jquery-3.6.0.min.js"></script>
+    <script src="../assets/vendors/bootstrap/js/bootstrap.bundle.min.js"></script>
---
+++ pages/components/header.php
@@ -0,0 +1,29 @@
+<header id="native-header">
+        <nav aria-label="first_header" class="navbar navbar-dark bg-dark">
+            <img id="company_name" src="../assets/img/logo.png" alt="InternQuest logo" width="300px">
+            <div id="button_div_header">
+                <button id="register_button" type="button" class="btn btn-primary" href="javascript:void(0)"><span>S'inscrire</span></button>
+                <button id="login_button" type="button" class="btn btn-primary" href="javascript:void(0)"><span>Se connecter</span></button>
+
+                <img id="account_avatar" src="../assets/img/avatar.svg" alt="Avatar image" width="50px">
+            </div>
+        </nav>
+        <nav aria-label="second_header" class="navbar navbar-expand-xl navbar-dark">
+            <div class="container-fluid">
+              <div class="collapse navbar-collapse show" id="navbarLight">
+                <ul class="navbar-nav me-auto mb-2 mb-xl-0">
+                  <li class="nav-item">
+                    <a class="nav-link active" aria-current="page" href="#">Offres de stage</a>
+                  </li>
+                  <li class="nav-item">
+                    <a class="nav-link" href="#"></a>
+                  </li>
+                  <li class="nav-item">
+                    <a class="nav-link" href="#">Features</a>
+                  </li>
+                </ul>
+                <a class="nav-link" href="#">Accueil recruteurs ➔</a>
+              </div>
+            </div>
+          </nav>
+    </header>
