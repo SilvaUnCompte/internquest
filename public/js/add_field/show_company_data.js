9e0676f194df0512d1c436ce6cb235104b796d2c
SilvaUnCompte <silvablocks@gmail.com>
2023-03-28 21:53:23 +0200

    modif h4 -> h6 dans recherhce

--- public/js/add_field/show_company_data.js
+++ public/js/add_field/show_company_data.js
@@ -7,14 +7,14 @@ function showCompanyData(identifiant) {
         let html = "";
         html += "<p id=\"identifiant\">"+identifiant+"</p>";
         html += "<h2>"+response.name+"</h2>";
-        html += "<h4>Confiance du pilote : "+response.pilot_trust+"</h3>";
-        html += "<h4>Note moyenne : "+ response.avrage +"</h3>";
-        html += "<h4>Secteur principal : </h3>";
-        html += "<h6>"+response.sectors[0]+"</p>"; 
-        html += "<h4>Localisation principal : </h3>";
+        html += "<h4>Confiance du pilote : "+response.pilot_trust+"</h4>";
+        html += "<h4>Note moyenne : "+ response.avrage +"</h4>";
+        html += "<h4>Secteur principal : </h4>";
+        html += "<h6>"+response.sectors[0]+"</h6>"; 
+        html += "<h4>Localisation principal : </h4>";
         html += "<h6>"+response.locations['street_address'] + ", " + response.locations['postal_code'] + " " + response.locations['city'] +"</p>"; 
-        html += "<h4>Description : </h3>";
-        html += "<h6>"+response.desc+"</p>"; 
+        html += "<h4>Description : </h4>";
+        html += "<h6>"+response.desc+"</h6>"; 
         html += "<h3>Notez cette entreprise : </br> <input id=\"grade-entry\" placeholder=\"/5\" name=\"grade\" type=\"number\" min=\"0\" max=\"5\"><button class=\"btn_model_animated\" id=\"grade-button\" onclick=\"addGrade()\">Noter</button></h3>"
 
         document.getElementById("card_infos").innerHTML = html;
--- public/js/add_field/show_offer_data.js
+++ public/js/add_field/show_offer_data.js
@@ -5,19 +5,19 @@ function showOfferData(identifiant) {
     xhr.onload = () => {
         let response = JSON.parse(xhr.responseText);
         let html = "";
-        html += "<p id=\"identifiant\">"+identifiant+"</p>";
+        html += "<h6 id=\"identifiant\">"+identifiant+"</h6>";
         html += "<h2>"+response.title+"</h2>";
-        html += "<h3>Entreprise : "+response.company_name+"</h3>"
-        html += "<h3> Adresse du lieu du stage : \n"+response.location.street_address+", "+response.location.city+", "+response.location.postal_code+"</h3>";
-        html += "<h3>Adresse courriel de contact : <a href=\"mailto:"+response.contactEmail+"\">"+response.contactEmail+"</a></h3>";
-        html += "<h3> Niveau scholaire minimum : "+response.lvl+"</h3>";
-        html += "<h3> Durée de stage : "+response.duration+" semaines </h3>";
-        html += "<h3> Salaire : "+response.remuneration+"€/mois </h3>";
-        html += "<h3>Description : </h3>";
-        html += "<p>"+response.desc+"</p>"; 
+        html += "<h4>Entreprise : "+response.company_name+"</h3>"
+        html += "<h4> Adresse du lieu du stage : \n"+response.location.street_address+", "+response.location.city+", "+response.location.postal_code+"</h3>";
+        html += "<h4>Adresse courriel de contact : <a href=\"mailto:"+response.contactEmail+"\">"+response.contactEmail+"</a></h3>";
+        html += "<h4> Niveau scholaire minimum : "+response.lvl+"</h3>";
+        html += "<h4> Durée de stage : "+response.duration+" semaines </h3>";
+        html += "<h4> Salaire : "+response.remuneration+"€/mois </h3>";
+        html += "<h4>Description : </h3>";
+        html += "<h6>"+response.desc+"</h6>"; 
         let n = 0;
         let skills = JSON.parse(response.skills);
-        html += "<h3> Les compétences requises : </h3>";
+        html += "<h4> Les compétences requises : </h3>";
         html += "<ul id=\"skills-list\">";
         while(skills[n] != undefined){
             html += "<li>"+skills[n]+"</li>";
